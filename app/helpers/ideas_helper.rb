9b6675da6f50a7277f0eb04d1b41022f354599eb
magda-gierg <mjgiergiczny@gmail.com>
2017-10-28 14:15:33 +1300

    filter is working

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,2 +1,8 @@
 module IdeasHelper
+  def naughty_filter(string)
+    if string.include?("poop")
+    return string.gsub("poop", "heck")
+  else return string
+    end
+  end
 end
--- spec/helpers/ideas_helper_spec.rb
+++ spec/helpers/ideas_helper_spec.rb
@@ -11,5 +11,22 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe IdeasHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "naughty_filter" do
+    it "filters poop" do
+      expect(naughty_filter("poop")).to eq ("heck")
+    end
+
+    it "does not filter nice words" do
+      expect(naughty_filter("I like cats")).to eq("I like cats")
+    end
+
+    it "filters out only naughty words" do
+      expect(naughty_filter("I like poop")).to eq("I like heck")
+    end
+
+    it "filters out many naughty words" do
+      expect(naughty_filter("poop poop")).to eq("heck heck")
+    end
+
+  end
 end
