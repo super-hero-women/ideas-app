6253db28dea290b3331ad4d458fce9d84f58ce4e
magda-gierg <mjgiergiczny@gmail.com>
2017-10-28 14:35:41 +1300

    cleanup

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -2,13 +2,9 @@ module IdeasHelper
   def naughty_filter(string)
     if string.include?("poop")
     string = string.gsub("poop", "heck")
-  else
-    string = string
     end
     if string.include?("butt")
     string = string.gsub("butt", "heck")
-  else
-    string = string
   end
     return string
   end
