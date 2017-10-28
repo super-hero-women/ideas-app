cd361fc489e58a31063fef1a7a6dd38c2e25e5f0
Elf <elf@Cheryls-MacBook-Pro-177.local>
2017-10-29 12:37:11 +1300

    comment order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028002617) do
+ActiveRecord::Schema.define(version: 20171028033317) do
 
   create_table "comments", force: :cascade do |t|
     t.text "description"
