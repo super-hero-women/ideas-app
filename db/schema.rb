95006b269da6731f520242e191e8d1901e3fbbfd
Elf <elf@Cheryls-MacBook-Pro-177.local>
2017-10-28 15:21:54 +1300

    Creating a database  Please enter the commit message for your changes. Lines starting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023084412) do
+ActiveRecord::Schema.define(version: 20171028002617) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.integer "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["idea_id"], name: "index_comments_on_idea_id"
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
